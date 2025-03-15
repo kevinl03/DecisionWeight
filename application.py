7a86d667b00cdf02dff81c90338cf44a9716c49f
kevinl03 <klitvin101@gmail.com>
2025-03-15 10:39:09 -0700

    bruh

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for,flash
 import sqlite3
 from datetime import datetime
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 def init_db():
     conn = sqlite3.connect('database.db')
